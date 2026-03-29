0e78a43aef69384347999bd7f8a13a9774992640
andrikotak27 <andrikotak27052001@gmail.com>
2026-03-29 21:04:17 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@
 const { kv } = require("@vercel/kv");
 
 // Discord Webhook URL
-const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1483298694391664702/3flhLEdabBBYWvYhsNkZ4WLvV1T4ZixUj13y8PELMh2xM2WSF28Li0x7NheZymEwz11z";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1487614437841178648/q88unsYbP1XMx1eEpIC5xXB3nhXh3JKotvIRMcOyK2Caizfi9duIz_5Xe0gz8GnjZH4D";
 
 async function redisCommand(args) {
   const url   = process.env.STORAGE_URL   || process.env.KV_REST_API_URL;
